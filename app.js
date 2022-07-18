e506439b86ca9377b86b975168ed6988606662ec
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-18 04:32:45 +0400

    custom types

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 function combine(var1, var2, resultType) {
     var result;
-    if (typeof var1 === 'number' && typeof var2 === 'number' || resultType === 'as-number') {
-        return result = +var1 + +var2;
+    if ((typeof var1 === "number" && typeof var2 === "number") ||
+        resultType === "as-number") {
+        return (result = +var1 + +var2);
     }
     else {
-        return result = var1.toString() + var2.toString();
+        return (result = var1.toString() + var2.toString());
     }
 }
 // that is the union types ( | )
 console.log(combine("marz", "sara", "as-text"));
-console.log(combine(18, 11, 'as-number'));
+console.log(combine(18, 11, "as-number"));
