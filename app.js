9961f2e2390c142efce2d9e00acaee2bd447342b
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-18 04:26:37 +0400

    literal types

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-function combine(var1, var2) {
+function combine(var1, var2, resultType) {
     var result;
-    if (typeof var1 === 'number' && typeof var2 === 'number') {
-        return result = var1 + var2;
+    if (typeof var1 === 'number' && typeof var2 === 'number' || resultType === 'as-number') {
+        return result = +var1 + +var2;
     }
     else {
         return result = var1.toString() + var2.toString();
     }
 }
-console.log(combine('marz', 'sara'));
-console.log(combine(18, 11));
+// that is the union types ( | )
+console.log(combine("marz", "sara", "as-text"));
+console.log(combine(18, 11, 'as-number'));
--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-function combine(var1: number | string, var2: number | string){
+function combine(var1: number | string, var2: number | string, resultType: 'as-text' | 'as-number'){
     let result;
-    if(typeof var1 === 'number' && typeof var2 === 'number'){
-       return result = var1 + var2;
+    if(typeof var1 === 'number' && typeof var2 === 'number' || resultType === 'as-number'){
+       return result = +var1 + +var2;
     }
     else{
         return result = var1.toString() + var2.toString();
     }
 }
+// that is the union types ( | )
 
-
-console.log(combine('marz','sara'));
-console.log(combine(18,11));
+console.log(combine("marz", "sara", "as-text"));
+console.log(combine(18,11, 'as-number'));
