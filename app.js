6d8197eec0efa91381141a1f0b30cb8bacf6ba26
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-19 03:36:03 +0400

    function types & callbacks

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ function printResult(num) {
     return ("result" + num);
 }
 ;
+function addAndHandler(num1, num2, cb) {
+    var result = num1 + num2;
+    cb(result);
+}
+;
+addAndHandler(10, 20, function (result) {
+    console.log(result);
+});
 printResult(add(5, 7));
 //this mean is, the variable combineValues just can be hold function which it get two parametr in number type and it mut be return  also number type value;
 var combineValues;
