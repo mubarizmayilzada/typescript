76c8dfd29929c9501e70a394b7d8f3a2655662a7
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-21 03:49:06 +0400

    rest operator

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// const add = (a: number, b: number = 1) => a + b;
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -9,17 +10,13 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-var add = function (a, b) {
-    if (b === void 0) { b = 1; }
-    return a + b;
-};
-var printOutput = function (output) { return console.log(output); };
-var button = document.querySelector('button');
-if (button) //because typescript doesn't know if button already exist or not we must be check that
- {
-    button.addEventListener('click', function (event) { return console.log(event); });
-}
-printOutput(add(5));
+// const printOutput: (a: string | number) => void = output => console.log(output); 
+// const button = document.querySelector('button');
+// if(button) //because typescript doesn't know if button already exist or not we must be check that
+// {
+//     button.addEventListener('click', event => console.log(event));
+// }
+// printOutput(add(5));
 var hobbies = ['Sports', 'Cooking'];
 var activeHobbies = ['hiking'];
 activeHobbies.push.apply(activeHobbies, hobbies);
@@ -28,3 +25,13 @@ var person = {
     age: 19
 };
 var copiedPerson = __assign({}, person);
+//or we can write this like that -> ...numbers: [number, number, number] but this time we can get just three value.
+var add = function () {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return numbers.reduce(function (a, b) { return a + b; }, 0);
+};
+var addedNumbers = add(5, 6, 7, 8, 34);
+console.log(addedNumbers);
--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-const printOutput: (a: string | number) => void = output => console.log(output); 
+// const printOutput: (a: string | number) => void = output => console.log(output); 
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-if(button) //because typescript doesn't know if button already exist or not we must be check that
-{
-    button.addEventListener('click', event => console.log(event));
-}
+// if(button) //because typescript doesn't know if button already exist or not we must be check that
+// {
+//     button.addEventListener('click', event => console.log(event));
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 
 
@@ -28,3 +28,13 @@ const person: {
 };
 
 const copiedPerson = {...person };
+
+
+//or we can write this like that -> ...numbers: [number, number, number] but this time we can get just three value.
+const add = (...numbers: number[]) => {
+    return  numbers.reduce((a, b) => a + b, 0);
+};
+
+const addedNumbers = add(5,6,7,8,34);
+
+console.log(addedNumbers);
