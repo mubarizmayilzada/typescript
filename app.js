45ee2790762f32f6152ec95838e2c4596adb5665
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-23 15:58:59 +0400

    readonly keyword for interface

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ var Person = /** @class */ (function () {
 }());
 var user1;
 user1 = new Person('Nisa');
+// user1.name = 'Sara'; //because of i wrote readonly infront of name property in interface.
 user1.greet('Hi there - I am');
 console.log(user1);
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 interface Greetable {
-    name: string;
+    readonly name: string;
 
     greet(phrase: string): void;
 }
@@ -19,5 +19,6 @@ class Person implements Greetable {
 let user1: Greetable;
 
 user1 = new Person('Nisa');
+// user1.name = 'Sara'; //because of i wrote readonly infront of name property in interface.
 user1.greet('Hi there - I am');
 console.log(user1);
