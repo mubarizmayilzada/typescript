f52f02454516be0c55d859e49652def11bec6f1f
mubarizmayilzade <mubarizbm@code.edu.az>
2022-07-24 19:01:07 +0400

    built-in generics

---
+++ app.ts
@@ -0,0 +1,12 @@
+////this is the generic type / mean is one type related to other types.
+// const arr: Array<string> = [];
+
+// const promise  = new Promise<string>((resolve, reject) =>{
+//     setTimeout(()=>{
+//         resolve('Marz')
+//     },2000) 
+// });
+
+// promise.then((data)=>{
+//     data.split(' ');
+// })
